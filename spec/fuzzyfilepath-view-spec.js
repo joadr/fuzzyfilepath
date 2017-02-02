875dff4c9b06d90eae1a86fae51c1f21fde56166
Joaquin Diaz <joaquin.diaz@ies.cl>
2017-02-02 15:58:24 -0300

    first commit

---
+++ spec/fuzzyfilepath-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FuzzyfilepathView from '../lib/fuzzyfilepath-view';
+
+describe('FuzzyfilepathView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
